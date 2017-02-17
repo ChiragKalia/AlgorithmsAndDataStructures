5915039d4a75ce3b821333083ffe39c6f495e427
ChiragKalia <chiragkalia1@gmail.com>
2017-02-17 12:11:34 +0530

    Added SortRectangularArray

---
+++ DataStructures/Arrays/Sort2DArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    public class Sort2DArray
+    {
+        //<Summary>
+        //A function that sorts a n*n array row by row
+        //</summary>
+        //<param name="length">Row and column size of array</param>
+        //<param name="A">n*n rectangular Array</param>
+        //<returns> Row by row sorted array</returns>
+        public int[,] sort2DArray(int length, ref int[,] A)
+        {
+            
+            return A;
+        }
+    }
+}
