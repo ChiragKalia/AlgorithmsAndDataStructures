6008bba8124edc8826859b97a1223299304cc6bf
chiragkalia <chiragkalia1@gmail.com>
2017-04-26 23:32:36 +0530

    Adding Quicksorter

---
+++ Algorithms/Sorting/QuickSorter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    public class QuickSorter
+    {
+        public int[] performQuickSort(int[] A, int n)
+        {
+            return A;
+        }
+    }
+}
