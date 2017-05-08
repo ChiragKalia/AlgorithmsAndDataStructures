e3f98c0451df8b24f850857002abeece2a04ebf2
chiragkalia <chiragkalia1@gmail.com>
2017-05-08 23:22:04 +0530

    Added CommonElements and RotateMatrix Class

---
+++ DataStructures/Arrays/CommonElements.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    public class CommonElements
+    {
+        //public int[] GetCommonElements(int[][] A)
+        //{
+        //    //int[] commonElements = new int[n];
+        //    int[] tempArr = null;
+        //    for (int i = 0; i < A.Count(); i++)
+        //    {
+
+        //        if (i == 0)
+        //        {
+        //            continue;
+        //        }
+        //        else
+        //        {
+        //            if (tempArr == null)
+        //                tempArr = A[i - 1];
+        //            else
+        //                tempArr = A[i].Intersect(tempArr).ToArray();
+        //        }               
+        //    }
+        //    return tempArr;
+        //}
+
+        public int[] GetCommonElements(int[,] A, int n)
+        {
+            int[] commonElements = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int[] tempArr = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0)
+                    {
+                        commonElements[j] = A[i, j];
+                    }
+                    else
+                    {
+                        tempArr[j] = A[i, j];
+                    }
+                }
+                if (i > 0)
+                {
+                    commonElements = commonElements.Intersect(tempArr).ToArray();
+                }
+            }
+            return commonElements;
+        }
+
+        public int[] GetCommonElementsUsingDictionary(int[,] A, int n)
+        {
+            int[] commonElements = new int[n];
+            Dictionary<int, int> commonElementsDict = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int arrValue = A[i, j];
+                    if (commonElementsDict.ContainsKey(arrValue))
+                    {
+                        commonElementsDict[arrValue] += 1; 
+                    }
+                    else
+                    {
+                        commonElementsDict.Add(arrValue, 1);
+                    }
+                }
+            }
+            commonElements = commonElementsDict.Where(i=>i.Value== n).Select(i=>i.Key).ToArray();
+            return commonElements;
+        }
+    }
+}
---
+++ DataStructures/Arrays/RotateMatrixClockwise.cs
@@ -0,0 +1,30 @@
+﻿using DataStructures.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    public class RotateMatrixClockwise
+    {
+        SwapVars swapClass;
+        public RotateMatrixClockwise()
+        {
+            swapClass = new SwapVars();
+        }
+        public int[] RotateMatrix(ref int[,] A, int n) // This method will rotate a matrix by 90degrees clockwise. //Params -1) Array 2) Size of matrix.
+        {
+            //Incomplete
+            int last = n - 1;
+            for(int i=0; i<last; i++)
+            {
+                swapClass.Swap(ref A[i,i], ref A[i,last]);
+                swapClass.Swap(ref A[i, i], ref A[last, last]);
+                swapClass.Swap(ref A[i, i], ref A[last, i]);
+            }
+            return A;
+        }
+    }
+}
