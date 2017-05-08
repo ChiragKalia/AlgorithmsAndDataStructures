2366545e243f7701ba8cdcacff440558a4fab293
chiragkalia <chiragkalia1@gmail.com>
2017-05-08 23:25:22 +0530

    Added SwapVars Helper Clas

---
+++ DataStructures/Helpers/SwapVars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Helpers
+{
+    public class SwapVars
+    {
+        public void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
