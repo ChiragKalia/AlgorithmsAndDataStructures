eb56ebed89eeb4a2713e2e196ba44020894d1210
chiragkalia <chiragkalia1@gmail.com>
2017-05-23 17:59:09 +0530

    Added Spherical Array Insertion (Incomplete)

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -22,9 +22,6 @@ namespace Algorithms.MainProgram
     {
         static void Main(string[] args)
         {
-
-            int n = 4;
-            int[,] A = new int[n,n];
             //for (int i =0; i< n; i++)
             //{
             //    string[] sArr = Console.ReadLine().Split(' ');
@@ -40,20 +37,20 @@ namespace Algorithms.MainProgram
             //{
             //    Console.WriteLine(" " + item);
             //}
-            int count = 1;
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    A[i, j] = count++;
-                }
-            }
-            Console.WriteLine(" Original Matrix: ");
-            PrintMatrix.Print2DMatrix(A, n);
-            Console.WriteLine(" Rotated Matrix: ");
-            RotateMatrixClockwise rotate = new RotateMatrixClockwise();
-            rotate.RotateMatrix(ref A, n);
-            PrintMatrix.Print2DMatrix(A, n);
+            //int count = 1;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        A[i, j] = count++;
+            //    }
+            //}
+            //Console.WriteLine(" Original Matrix: ");
+            //PrintMatrix.Print2DMatrix(A, n);
+            //Console.WriteLine(" Rotated Matrix: ");
+            //RotateMatrixClockwise rotate = new RotateMatrixClockwise();
+            //rotate.RotateMatrix(ref A, n);
+            //PrintMatrix.Print2DMatrix(A, n);
 
             //Sort Binary Array
             //BinaryArraySort sortBinary = new BinaryArraySort();
@@ -63,6 +60,11 @@ namespace Algorithms.MainProgram
             //A = sortBinary.sortBinaryArray(A, n);
             //Console.WriteLine(" Sorted Matrix: ");
             //PrintMatrix.Print1DMatrix(A, n);
+            int n = 6;
+            int[,] A = new int[n,n];
+            SphericalArrayInsertion insertion = new SphericalArrayInsertion();
+            insertion.InsertValuesSpherically(ref A, n);
+            PrintMatrix.Print2DMatrix(A, n);
             Console.ReadLine();
         }
 
