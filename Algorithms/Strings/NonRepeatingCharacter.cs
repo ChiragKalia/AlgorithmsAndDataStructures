2fa085bcf8f3b45f9e44d59b7b7d8cc9d4ec6341
chiragkalia <chiragkalia1@gmail.com>
2020-03-10 18:14:23 -0400

    Added NonRepeatingCharacter.cs to Strings Test

---
+++ Algorithms/Strings/NonRepeatingCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Strings
+{
+    public class NonRepeatingCharacter
+    {
+        static int numberOfCharacters = 256;
+        static char[] count = new char[numberOfCharacters];
+        static void GetCharCountArray(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+                count[str[i]]++;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (count[str[i]] == 1)
+                {
+                    Console.WriteLine("First Unique Character in the String Was " + str[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
