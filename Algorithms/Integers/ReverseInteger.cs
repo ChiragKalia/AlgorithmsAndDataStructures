3c1ec2eba7480d50c3676b48c8a95b69f5291396
Chirag Kalia <chiragkalia1@gmail.com>
2023-10-01 00:23:07 -0400

    Added new Files and Updated .NET Version

---
+++ Algorithms/Integers/ReverseInteger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Integers
+{
+    public class ReverseInteger
+    {
+        public static int Reverse(int x)
+        {
+            int result = 0;
+            if (x > Int32.MaxValue || x < Int32.MinValue)
+            {
+                return result;
+            }
+            while (x != 0)
+            {
+                int pop = x % 10;
+                x /= 10;
+                if (pop > Int32.MaxValue-1%10)
+                    return 0;
+                if (pop < Int32.MinValue%10)
+                    return 0;
+                result = result * 10 + pop;
+            }
+            return result;
+        }
+    }
+}
--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -15,6 +15,7 @@ using static DataStructures.Trees.BinarySearchTree;
 using Algorithms.Sorting;
 using DataStructures.Helpers;
 using Algorithms.Strings;
+using Algorithms.Integers;
 //using DataStructures.LinkedLists;
 
 namespace Algorithms.MainProgram
@@ -23,104 +24,16 @@ namespace Algorithms.MainProgram
     {
         static void Main(string[] args)
         {
-            //int n = 10;
-            //int[,] A = new int[n, n];
-            //for (int i =0; i< n; i++)
-            //{
-            //    string[] sArr = Console.ReadLine().Split(' ');
-            //    int[] arr = Array.ConvertAll(sArr, Int32.Parse);
-            //    for (int j = 0; j < arr.Length; j++)
-            //    {
-            //        A[i,j] = arr[j];
-            //    }
-            //}
-            //CommonElements elements = new CommonElements();
-            //int[] output = elements.GetCommonElementsUsingDictionary(A,n);
-            //foreach (var item in output)
-            //{
-            //    Console.WriteLine(" " + item);
-            //}
-
-            //int count = 1;
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        A[i, j] = count++;
-            //    }
-            //}
-            //Console.WriteLine(" Original Matrix: ");
-            //PrintMatrix.Print2DMatrix(A, n);
-            //Console.WriteLine(" Rotated Matrix: ");
-            //RotateMatrixClockwise rotate = new RotateMatrixClockwise();
-            //rotate.RotateMatrix(ref A, n);
-            //PrintMatrix.Print2DMatrix(A, n);
-
-            //Sort Binary Array
-            //BinaryArraySort sortBinary = new BinaryArraySort();
-            //Console.WriteLine(" Original Matrix: ");
-            //PrintMatrix.Print1DMatrix(A, n);
-            //Console.WriteLine();
-            //A = sortBinary.sortBinaryArray(A, n);
-            //Console.WriteLine(" Sorted Matrix: ");
-            //PrintMatrix.Print1DMatrix(A, n);
-
-            //Spherical Array Insertion
-
-            //int n = 10;
-            //int[,] A = new int[n, n];
-            //SpiralArrayInsertion insertion = new SpiralArrayInsertion();
-            //insertion.InsertValuesSpirally(ref A, n);
-            //PrintMatrix.Print2DMatrix(A, n);
-
-            //Maximum Hourglass Sum
-            //int[][] arr = new int[6][];
-            //for (int arr_i = 0; arr_i < 6; arr_i++)
-            //{
-            //    string[] arr_temp = Console.ReadLine().Split(' ');
-            //    arr[arr_i] = Array.ConvertAll(arr_temp, Int32.Parse);
-            //}
-            //MaximumHourglassSum hourGlassSum = new MaximumHourglassSum();
-            //int maxSum = hourGlassSum.GetMaximumHourglassSum(arr);
-            //Console.WriteLine("Max Sum: " + maxSum);
-            //Max Sum Subarray of a square matrix
-            //int n = 3;
-            //int[,] A = new int[n,n];
-            //for (int i = 0; i < n; i++)
-            //{
-            //    string[] arr_temp = Console.ReadLine().Split(' ');
-            //    int[] currentIteration = Array.ConvertAll(arr_temp, Int32.Parse);
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        A[i, j] = currentIteration[j];
-            //    }
-            //}
-            //MaxSumSubarraySquareMatrix maxSum = new MaxSumSubarraySquareMatrix();
-            //int maxValue = maxSum.CalculateMaxSum(A, n);
-            //Console.WriteLine(maxValue);
-
-            //int n = 7;
-            //int[] A = {1,2,3,1,3,6,6};
-            //RepetitiveElementsArray repititiveElements = new RepetitiveElementsArray();
-            //repititiveElements.PrintRepetitiveElementsInArray(A, n);
-            //Console.ReadLine();
-
-            ////Merge Sort
-            //int[] arr = new int[] { 2, 4, 1, 6, 8, 5, 3, 7 };
-            //MergeSorter.MergeSort(arr);
-            //foreach (int i in arr)
-            //{
-            //    Console.Write(i);
-            //}
-
-            ////Check If Anagram
-            //string s1 = "Peeks";
-            //string s2 = "Keep";
-            //Console.WriteLine(Anagram.CheckIfAnagram(s1, s2));
-
-
+            Program program = new Program();
+            Console.WriteLine(program.isPower);
             Console.ReadLine();
         }
-
+        public int isPower(int A)
+        {
+            if(Math.Sqrt(A) )
+            return 0;
+        }
     }
+
+}
 }
