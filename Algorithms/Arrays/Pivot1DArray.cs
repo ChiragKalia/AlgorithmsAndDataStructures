04b943b3f0c1ae021263e6d014841f232accdde6
Chirag Kalia <chiragkalia1@gmail.com>
2023-10-01 13:20:22 -0400

    Added new Pivot 1D Array Class

---
+++ Algorithms/Arrays/Pivot1DArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Arrays
+{
+    public class Pivot1DArray
+    {
+        public int[] RotateArrayRight(int[] x, int pivot)
+        {
+            int[] r = x.Skip(pivot).Concat(x.Take(pivot)).ToArray();
+            return r;
+        }
+    }
+}
