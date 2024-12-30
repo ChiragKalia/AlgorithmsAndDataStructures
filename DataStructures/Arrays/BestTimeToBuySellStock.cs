8f1f5528236cc1380cd5abb010230ba55f8e770e
chiragkalia1 <chiragkalia1@gmail.com>
2024-12-30 14:22:17 -0500

    Added Max Profit Class

---
+++ DataStructures/Arrays/BestTimeToBuySellStock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    public class BestTimeToBuySellStock
+    {
+        int MaxProfit(int[] arr)
+        {
+            int maxPro = 0, minPrice = Int32.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                minPrice = Math.Min(minPrice, arr[i]);
+                maxPro = Math.Max(maxPro, arr[i] - minPrice);
+            }
+            return maxPro;
+        }
+    }
+}
--- DataStructures/Arrays/Sort012NumbersArray.cs
+++ DataStructures/Arrays/Sort012NumbersArray.cs
@@ -10,32 +10,34 @@ namespace DataStructures.Arrays
     {
         /*
         Problem Statement: Given an array consisting of only 0s, 1s, and 2s. Write a program to in-place sort the array without using inbuilt sort functions ( Expected: Single pass-O(N) and constant space) 
+        https://leetcode.com/problems/sort-colors/
         https://takeuforward.org/data-structure/sort-an-array-of-0s-1s-and-2s/
         https://www.youtube.com/watch?v=tp8JIuCXBaU&list=PLgUwDviBIf0oF6QL8m22w1hIDC1vJ_BHz&index=22
          */
 
-        public static int[] Sort012Array(int[] arr)
+        public static int[] SortColors(int[] arr)
         {
-            int n = arr.Length;
-            int low = 0;
-            int mid = 0;
-            int high = n - 1;
-
+            int low = 0, mid = 0, high = arr.Length;
             while (mid <= high)
             {
-                switch (arr[mid])
+                if (arr[mid] == 0)
                 {
-                    case 0:
-                        (arr[low], arr[mid]) = (arr[mid], arr[low]);
-                        low++; mid++;
-                        break;
-                    case 1:
-                        mid++;
-                        break;
-                    case 2:
-                        (arr[mid], arr[high]) = (arr[high], arr[mid]);
-                        high--;
-                        break;
+                    int temp = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp;
+                    low++;
+                    mid++;
+                }
+                else if (arr[mid] == 1)
+                {
+                    mid++;
+                }
+                else
+                {
+                    int temp = arr[high];
+                    arr[high] = arr[mid];
+                    arr[mid] = temp;
+                    high--;
                 }
             }
             return arr;
