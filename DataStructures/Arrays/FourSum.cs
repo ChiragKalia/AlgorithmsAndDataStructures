b9e3025db9a5eabf82dedc4c6e35f4ebc8f8c80f
chiragkalia1 <chiragkalia1@gmail.com>
2025-01-06 02:28:41 -0500

    Added Three and Four Sum Problems(Two Pointer Optimal Approach)

---
+++ DataStructures/Arrays/FourSum.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    public class FourSum
+    {
+        /*
+        *Given an array of N integers, your task is to find unique quads that add up to give a target value. In short, you need to return an array of all the unique quadruplets [arr[a], arr[b], arr[c], arr[d]] such that their sum is equal to a given target.
+        * https://leetcode.com/problems/4sum/
+        * https://takeuforward.org/data-structure/4-sum-find-quads-that-add-up-to-a-target-value/
+        */
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            // List to store the resulting quadruplets
+            IList<IList<int>> result = new List<IList<int>>();
+            // Sort the array to facilitate the two-pointer approach and handle duplicates
+            Array.Sort(nums);
+            // Outer loop for the first element of the quadruplet
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // Skip duplicates for the first element
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                // Second loop for the second element of the quadruplet
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    // Skip duplicates for the second element
+                    if (j != i + 1 && nums[j] == nums[j - 1]) continue;
+                    // Initialize two pointers for the remaining two elements
+                    int k = j + 1; int l = nums.Length - 1;
+                    // Use the two-pointer technique to find pairs that sum to target
+                    while (k < l)
+                    {
+                        // Calculate the current sum
+                        long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                        if (sum == target)
+                        {
+                            // Add the quadruplet to the result if the sum matches the target
+                            result.Add(new List<int> { nums[i], nums[j], nums[k], nums[l] });
+
+                            // Move pointers to the next unique elements
+                            k++;
+                            l--;
+                            while (k < l && nums[k] == nums[k - 1]) k++;
+                            while (k < l && nums[l] == nums[l + 1]) l--;
+                        }
+                        else if (sum < target)
+                        {
+                            // Move the left pointer to increase the sum
+                            k++;
+                        }
+                        else
+                        {
+                            // Move the right pointer to decrease the sum
+                            l--;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ DataStructures/Arrays/ThreeSum.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    public class ThreeSum
+    {
+        /*
+        * Problem Statement: Given an array of N integers, your task is to find unique triplets that add up to give a sum of zero. In short, you need to return an array of all the unique triplets [arr[a], arr[b], arr[c]] such that i!=j, j!=k, k!=i, and their sum is equal to zero.
+        * https://leetcode.com/problems/3sum/
+        * https://takeuforward.org/data-structure/3-sum-find-triplets-that-add-up-to-a-zero/
+        */
+        public IList<IList<int>> FindThreeSum(int[] nums)
+        {
+            // List to store the resulting triplets.
+            IList<IList<int>> result = new List<IList<int>>();
+
+            // Sort the array to enable the two-pointer approach and handle duplicates.
+            Array.Sort(nums);
+
+            // Iterate through the array to pick the first element of the triplet.
+            for (int i = 0; i < nums.Length; i++)  // Only go up to nums.Length - 2
+            {
+                // Skip duplicate elements to avoid duplicate triplets.
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                // Initialize two pointers: 'j' starts after 'i', and 'k' starts at the end.
+                int j = i + 1;
+                int k = nums.Length - 1;
+
+                // Use the two-pointer approach to find valid triplets.
+                while (j < k)
+                {
+                    // Calculate the sum of the triplet.
+                    long sum = (long)nums[i] + nums[j] + nums[k];
+
+                    if (sum == 0)
+                    {
+                        // If the sum equals zero, add the triplet to the result list.
+                        result.Add(new List<int> { nums[i], nums[j], nums[k] });
+
+                        // Move both pointers to the next unique elements to avoid duplicates.
+                        j++;
+                        k--;
+                        while (j < k && nums[j] == nums[j - 1]) j++;
+                        while (j < k && nums[k] == nums[k + 1]) k--;
+                    }
+                    else if (sum < 0)
+                    {
+                        // If the sum is less than zero, move the 'j' pointer right to increase the sum.
+                        j++;
+                    }
+                    else
+                    {
+                        // If the sum is greater than zero, move the 'k' pointer left to decrease the sum.
+                        k--;
+                    }
+                }
+            }
+            // Return the list of triplets.
+            return result;
+        }
+    }
+}
