f56511144b0bfe436dba71dfbd45f94440c8f55e
chiragkalia1 <chiragkalia1@gmail.com>
2025-01-07 01:16:59 -0500

    Added Longest Consecutive Sequence

---
+++ DataStructures/Arrays/LongestConsecutiveSequence.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    public class LongestConsecutiveSequence
+    {
+        /*
+        *Problem Statement: You are given an array of ‘N’ integers. You need to find the length of the longest sequence which contains the consecutive elements.
+        * https://leetcode.com/problems/longest-consecutive-sequence/
+        * https://takeuforward.org/data-structure/longest-consecutive-sequence-in-an-array/
+        */
+        public int LongestConsecutive(int[] nums)
+        {
+            // If the input array is empty, the longest consecutive sequence is 0.
+            if (nums.Length == 0)
+                return 0;
+
+            // Variable to store the longest consecutive sequence length.
+            int longest = 0;
+
+            // Use a HashSet to store the numbers for O(1) lookup.
+            HashSet<int> set = new HashSet<int>(nums);
+
+            // Iterate through each number in the HashSet.
+            foreach (int num in set)
+            {
+                // Check if the current number is the start of a sequence.
+                // A number is the start of a sequence if `num - 1` is not in the set.
+                if (!set.Contains(num - 1))
+                {
+                    // Initialize a counter for the current sequence.
+                    int cnt = 1;
+
+                    // Check for subsequent numbers in the sequence.
+                    while (set.Contains(num + cnt))
+                        cnt++;
+
+                    // Update the longest sequence length if the current sequence is longer.
+                    longest = Math.Max(cnt, longest);
+                }
+            }
+
+            // Return the length of the longest consecutive sequence.
+            return longest;
+        }
+    }
+}
