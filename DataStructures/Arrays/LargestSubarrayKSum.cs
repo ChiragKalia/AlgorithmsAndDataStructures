f5485fd5b24fe970e5e1a61d219203561eb8354b
chiragkalia1 <chiragkalia1@gmail.com>
2025-01-15 00:00:57 -0500

    Update comments

--- DataStructures/Arrays/LargestSubarrayKSum.cs
+++ DataStructures/Arrays/LargestSubarrayKSum.cs
@@ -15,17 +15,17 @@ namespace DataStructures.Arrays
         public int LargestSubarrayWithSumKPositives(int[] nums, int target)
         {
             int max = 0, currSum = nums[0];
-            int i = 0, j = 0;
-            while (j < nums.Length)
+            int i = 0, j = 0; //Two pointer approach
+            while (j < nums.Length) //Only move right if current sum is less than target
             {
-                while (i <= j && currSum > target)
+                while (i <= j && currSum > target) //Reduce size of subarray from the left if the currentsum surpasses target
                 {
                     currSum -= nums[i];
                     i++;
                 }
-                if (currSum == target)
+                if (currSum == target) 
                 {
-                    max = Math.Max(max, (j - i + 1));
+                    max = Math.Max(max, (j - i + 1)); // Update max if current subarray is larger than existing max.
                 }
                 j++;
                 if (j < nums.Length) currSum += nums[j];
@@ -36,16 +36,13 @@ namespace DataStructures.Arrays
         public int LargestSubarrayWithSumK(int[] nums, int sum)
         {
             int max = 0, currSum = 0;
-            Dictionary<int, int> prefixSumDict = new Dictionary<int, int>();
+            Dictionary<int, int> prefixSumDict = new Dictionary<int, int>(); //Store Prefix sums.
             for (int i = 0; i < nums.Length; i++)
             {
                 currSum += nums[i];
                 if (currSum == sum)
                 {
                     max = Math.Max(max, i + 1);
-                    if (!prefixSumDict.ContainsKey(currSum))
-                    {
-                    }
                 }
                 else if (prefixSumDict.ContainsKey(currSum - sum))
                 {
