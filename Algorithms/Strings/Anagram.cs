ec4c184330f69a3060db7174a5330ad00123b7c8
chiragkalia1 <chiragkalia1@gmail.com>
2025-12-12 14:16:49 -0500

    Updated Anagrams problem

--- Algorithms/Strings/Anagram.cs
+++ Algorithms/Strings/Anagram.cs
@@ -8,6 +8,9 @@ namespace Algorithms.Strings
 {
     public class Anagram
     {
+        //Sorting the characters of both strings
+        //Complexity: O(n log n)
+        // Because sorting the characters of the strings takes O(n log n) time.
         public static bool CheckIfAnagram(string s1, string s2)
         {
             char[] c1 = s1.ToLower().ToCharArray();
@@ -25,5 +28,45 @@ namespace Algorithms.Strings
             }
             return false;
         }
+
+        //Using a dictionary to count character occurrences.
+        bool AreAnagrams(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+            foreach (char c in s1.ToLower())
+            {
+                if (charCounts.ContainsKey(c))
+                {
+                    charCounts[c]++;
+                }
+                else
+                {
+                    charCounts[c] = 1;
+                }
+            }
+
+            foreach (char c in s2.ToLower())
+            {
+                if (!charCounts.ContainsKey(c))
+                {
+                    return false;
+                }
+                charCounts[c]--;
+
+                if (charCounts[c] == 0)
+                {
+                    charCounts.Remove(c);
+                }
+            }
+
+            return charCounts.Count == 0;
+        }
+
     }
 }
