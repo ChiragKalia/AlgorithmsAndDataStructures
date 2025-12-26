8c6f207f224b612bdb4a0d6913b1ef93fe0f73cb
chiragkalia1 <chiragkalia1@gmail.com>
2025-12-25 22:33:58 -0500

    Updated Missing Repeating Number (Cyclic Sort)

--- DataStructures/Arrays/FindMissingRepeatedNumbers.cs
+++ DataStructures/Arrays/FindMissingRepeatedNumbers.cs
@@ -15,20 +15,34 @@ namespace DataStructures.Arrays
          */
         public static int[] FindMissingRepeatingNumber(int[] nums)
         {
-            long n = nums.Length;
-            //S - SN (here we are taking the difference between sum of numbers in
-            //the array and sum of n natural numbers) 
-            long S = nums.Sum(), SN = (n * (n + 1)) / 2;
-            //S2 - S2N (difference of square sum of numbers in array and sum of
-            //square of n natural numbers)
-            long S2 = nums.Sum(z=>z*z), S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+            int i = 0;
+            while (i < nums.Length)
+            {
+                int correctIndex = nums[i] - 1;
 
-            long val1 = S - SN; //x - y
-            long val2 = S2 - S2N;
-            val2 = val2 / val1; // x + y
-            long x = (int)(val1 + val2) / 2;
-            long y = x - val1;
-            return new int[] { (int)x, (int)y };
+                // Condition 1: Is the number NOT in its right home?
+                // Condition 2: Is its home NOT already occupied by its twin?
+                if (nums[i] != nums[correctIndex])
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[correctIndex];
+                    nums[correctIndex] = temp;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != j + 1)
+                {
+                    // ALWAYS return the VALUE first, then the EXPECTED value
+                    return new int[] { nums[j], j + 1 };
+                }
+            }
+            return new int[0];
         }
     }
 }
