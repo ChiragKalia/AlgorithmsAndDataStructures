30471bdeb83ccb2d1ecc93b78b550223b8d5e15a
chiragkalia1 <chiragkalia1@gmail.com>
2026-01-18 10:19:44 -0500

    Arrays: Rotate Array by K places

--- DataStructures/Arrays/RotateArray.cs
+++ DataStructures/Arrays/RotateArray.cs
@@ -20,5 +20,24 @@ namespace DataStructures.Arrays
                 arr[n - 1] = swap;
             }
         }
+        /// <summary>
+        /// Rotates the elements of the specified array to the right by the specified number of places.
+        /// </summary>
+        /// <remarks>The rotation is performed in-place, modifying the input array. If k is 0 or equal to
+        /// the length of the array, the array remains unchanged.</remarks>
+        /// <param name="nums">The array of integers to rotate. Cannot be null.</param>
+        /// <param name="k">The number of positions to rotate the array to the right. Must be between 0 and the length of the array,
+        /// inclusive.</param>
+        /// <returns>An array containing the rotated elements. If the array has fewer than two elements, the original array is
+        /// returned.</returns>
+        public int[] RotateArrayByKPlaces(int[] nums, int k)
+        {
+            int n = nums.Length;
+            if (nums.Length < 2) return nums;
+            Array.Reverse(nums, 0, n - k);
+            Array.Reverse(nums, n - k, k);
+            Array.Reverse(nums);
+            return nums;
+        }
     }
 }
